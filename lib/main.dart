eca450be6364f44e952459eda68ab529fb788266
Chinmay <chaudhari.chinmay12345@gmail.com>
2024-05-15 13:19:22 +0530

    Added permissions

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:taskwarrior/app/themes/themes.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Permission.notification.isDenied.then((value) {
+    if (value) {
+      Permission.notification.request();
+    }
+  });
   runApp(
     GetMaterialApp(
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
-        theme: lightTheme,
-        darkTheme: darkTheme,
+      theme: lightTheme,
+      darkTheme: darkTheme,
     ),
   );
 }
